671cc617d48890d4a57089990d77d6210cfc1d2b
Yuxuan <nine.slide@gmail.com>
2023-09-24 20:48:27 +0200

    add linegradient file

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import useMediaQuery from './hooks/useMediaQuery';
 import Navbar from './scenes/Navbar';
 import DotGroup from './scenes/DotGroup';
 import Landing from './scenes/Landing';
+import LineGradient from './components/LineGradient';
 
 function App() {
     const [selectedPage, setSelectedPage] = useState('home');
@@ -39,6 +40,10 @@ function App() {
                 )}
                 <Landing setSelectedPage={setSelectedPage} />
             </div>
+			<LineGradient/>
+			<div className='w-5/6 mx-auto md:h-full'>
+
+			</div>
         </div>
     );
 }
